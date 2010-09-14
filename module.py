ee400d2af14c01bd88c225030baee95e43c7d658
RecursiveGreen <recursive.green@gmail.com>
2010-09-14 16:27:57 -0400

    Updated Channel object, added MixPlugin object

--- module.py
+++ module.py
@@ -14,6 +14,8 @@ class Sample(object):
         self.volume = 256
         self.globalvol = 64
         self.flags = 0
+        self.reltone = 0
+        self.finetune = 0
         self.vibtype = 0
         self.vibrate = 0
         self.vibdepth = 0
@@ -135,6 +137,7 @@ class Instrument(object):
         self.midibank = -1
         self.midiprogram = -1
         self.midichannelmask = 0
+        self.mididrumkey = 0
         self.pitchpansep = 0
         self.pitchpancenter = 60
         self.name = ''
@@ -147,10 +150,140 @@ class Instrument(object):
 
 class Channel(object):
     """A simple channel object"""
-    def __init__(self, panning=0, volume=0, flags=0):
-        self.panning = panning
-        self.volume = volume
+    def __init__(self):
+        # Most used mixing info
+        self.cursampledata = []
+        self.pos = 0
+        self.poslo = 0
+        self.inc = 0
+        self.rightvol = 0
+        self.leftvol = 0
+        self.rightramp = 0
+        self.leftramp = 0
+        
+        # Other info. . .
+        self.length = 0
         self.flags = flags
+        self.loopbegin = 0
+        self.loopend = 0
+        self.ramprightvol = 0
+        self.rampleftvol = 0
+        self.filterY1 = 0
+        self.filterY2 = 0
+        self.filterY3 = 0
+        self.filterY4 = 0
+        self.filterA0 = 0
+        self.filterB0 = 0
+        self.filterB1 = 0
+        self.rofs = 0
+        self.lofs = 0
+        self.ramplength = 0
+        self.sampledata = []
+        self.newrightvol = 0
+        self.newleftvol = 0
+        self.realvolume = 0
+        self.realpanning = 0
+        self.volume = volume
+        self.panning = panning
+        self.fadeoutvol = 0
+        self.period = 0
+        self.c4speed = 0
+        self.portamentodest = 0
+        self.instrument = Instrument()
+        self.sample = Sample()
+        self.volenvpos = 0
+        self.panenvpos = 0
+        self.pitchenvpos = 0
+        self.masterchn = 0
+        self.vumeter = 0
+        self.globalvol = 0
+        self.instvol = 0
+        self.finetune = 0
+        self.transpose = 0
+        self.portamentoslide = 0
+        self.autovibdepth = 0
+        self.autovibpos = 0
+        self.vibratopos = 0
+        self.tremolopos = 0
+        self.panbrellopos = 0
+        
+        self.volswing = 0
+        self.panswing = 0
+        
+        self.note = 0
+        self.nna = 0
+        self.newnote = 0
+        self.newinst = 0
+        self.command = 0
+        self.arpeggio = 0
+        self.oldvolumeslide = 0
+        self.oldfinevolupdown = 0
+        self.oldportaupdown = 0
+        self.oldfineportaupdown = 0
+        self.oldpanslide = 0
+        self.oldchnvolslide = 0
+        self.vibratotype = 0
+        self.vibratospeed = 0
+        self.vibratodepth = 0
+        self.tremolotype = 0
+        self.tremolospeed = 0
+        self.tremolodepth = 0
+        self.panbrellotype = 0
+        self.panbrellospeed = 0
+        self.panbrellodepth = 0
+        self.oldcmdex = 0
+        self.oldvolparam = 0
+        self.oldtempo = 0
+        self.oldoffset = 0
+        self.oldhioffset = 0
+        self.cutoff = 0
+        self.resonance = 0
+        self.retrigcount = 0
+        self.retrigparam = 0
+        self.tremorcount = 0
+        self.tremorparam = 0
+        self.patternloop = 0
+        self.patternloopcount = 0
+        self.rownote = 0
+        self.rowinst = 0
+        self.rowvolcmd = 0
+        self.rowvol = 0
+        self.rowcmd = 0
+        self.rowparam = 0
+        self.leftvu = 0
+        self.rightvu = 0
+        self.activemacro = 0
+        self.padding = 0
+
+
+class ChannelSettings(object):
+    """An object for storing channel settings"""
+    def __init__(self):
+        self.panning = 0
+        self.volume = 0
+        self.flags = 0
+        self.mixplugin = 0
+        self.name = ''
+
+
+class MixPlugin(object):
+    """An object for storing plugin data (VSTi)"""
+    def __init__(self):
+        self.flags = 0
+        self.voldecayleft = 0
+        self.voldecayright = 0
+        self.mixbuffer = 0
+        self.outbufferleft = 0.0
+        self.outbufferright = 0.0
+        self.plugindatasize = 0
+        self.plugindata = []
+        self.pluginid1 = 0
+        self.pluginid2 = 0
+        self.inputrouting = 0
+        self.outputrouting = 0
+        self.RESERVED = 0
+        self.name = ''
+        self.libraryname = ''
 
 
 class Note(object):
